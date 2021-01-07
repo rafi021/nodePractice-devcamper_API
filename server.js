263b7fce7600f676e81482a8a6088f05e7c4ff86
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2021-01-07 16:49:08 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//connect to database
+connectDB();
+
+//Routes files
+const bootcampRoute = require('./routes/bootcampsRoutes');
+
+//Initialize APP 
+const app = express();
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcampRoute);
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close( () => process.exit(1) )
+});
